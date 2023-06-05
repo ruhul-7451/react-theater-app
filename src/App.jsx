7566a9df43d52cad08eda5518f42a01be357ea9e
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-05 23:29:16 +0600

    redux toolkit initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState,useEffect } from "react"
-import {fetchDataFromApi} from "./utils/api"
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const url = useSelector((state) => state.url);
+  useEffect(() => {
+    testDataFetch();
+  }, []);
 
-  useEffect(()=>{
-    testDataFetch()
-  },[]);
+  const testDataFetch = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
 
-const testDataFetch=()=>{
-  fetchDataFromApi("/movie/popular").then((res)=>{
-    console.log(res)
-  })
+  return <div className="App">App {url?.total_pages}</div>;
 }
 
-
-  return (
-      <div  className='App'>App</div>
-  )
-}
-
-export default App
+export default App;
