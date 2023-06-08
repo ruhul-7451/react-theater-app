8750256f5094c8bd1cbb66c305a1a11750259a5d
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-08 21:57:59 +0600

    switch tab added in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 const Home = () => {
   return (
     <div>
       <HeroBanner />
+      <Trending />
       <div style={{ height: 1000 }}></div>
     </div>
   );
