1b3bed44756d7fd4aed41d7709867475b0f5f97e
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-12 21:04:23 +0600

    slider and navigation part completed

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -18,7 +18,18 @@ const Carousel = ({ data, loading }) => {
   //   console.log(carouselContainer.current);
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth - 20)
+        : container.scrollLeft + (container.offsetWidth - 20);
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
   const skItem = () => {
     return (
@@ -44,14 +55,20 @@ const Carousel = ({ data, loading }) => {
           onClick={() => navigation("right")}
         />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path
                 ? url.poster + item.poster_path
                 : PosterFallback;
 
               return (
-                <div key={item.id} className="carouselItem">
+                <div
+                  key={item.id}
+                  className="carouselItem"
+                  onClick={() => {
+                    navigate(`/${item.media_type}/${item.id}`);
+                  }}
+                >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />\
