4354abb6e983cb6592087ccedfe7c8338e719db2
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-12 20:53:54 +0600

    sliced genres to 2

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -55,7 +55,7 @@ const Carousel = ({ data, loading }) => {
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />\
-                    <Genres data={item.genre_ids} />
+                    <Genres data={item.genre_ids.slice(0, 2)} />
                   </div>
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
