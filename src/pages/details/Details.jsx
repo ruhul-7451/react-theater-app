1e48ee2444677b2d6842a2f1d1b2874334ed1e1e
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-13 00:05:01 +0600

    DetailsBanner - primary visual done

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./style.scss";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 const Details = () => {
-  return <div>Details</div>;
+  // const { mediaType, id } = useParams();
+  // const { data, loading } = useFetch(`/${mediaType}/${id}`);
+  return (
+    <div>
+      <DetailsBanner />
+    </div>
+  );
 };
-
 export default Details;
