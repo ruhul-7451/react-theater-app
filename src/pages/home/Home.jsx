7dfde6b1a67d7fff7fbe4b3ea8530fec45eedd9a
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-12 22:48:31 +0600

    most popular part added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <Trending />
+      <Popular />
       <div style={{ height: 1000 }}></div>
     </div>
   );
