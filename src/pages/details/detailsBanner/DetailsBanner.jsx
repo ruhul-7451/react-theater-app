6327e2afd439d24763deab65a1cf76c098570d17
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-13 00:42:32 +0600

    details page write part completed

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -13,11 +13,14 @@ import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "./Playbtn";
 // import VideoPopup from "../../../components/videoPopup/VideoPopup";
 import Img from "../../../components/lazyLoadImg/img";
-const DetailsBanner = () => {
+const DetailsBanner = ({ video, crew }) => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
   const { url } = useSelector((state) => state.home);
-
+  const director = crew?.filter((f) => f.job === "Director");
+  const writer = crew?.filter(
+    (f) => f.job === "Screenplay" || f.job === "Story" || f.job === "Writer"
+  );
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
@@ -90,6 +93,51 @@ const DetailsBanner = () => {
                         </div>
                       )}
                     </div>
+                    {director?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Director:{""}</span>
+                        <span className="text">
+                          {director?.map((drc, id) => {
+                            return (
+                              <span key={id}>
+                                {drc.name}
+                                {director.length - 1 !== id && ", "}
+                              </span>
+                            );
+                          })}
+                        </span>
+                      </div>
+                    )}
+                    {writer?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Writer:{""}</span>
+                        <span className="text">
+                          {writer?.map((drc, id) => {
+                            return (
+                              <span key={id}>
+                                {drc.name}
+                                {writer.length - 1 !== id && ", "}
+                              </span>
+                            );
+                          })}
+                        </span>
+                      </div>
+                    )}
+                    {data?.created_by?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Creator:{""}</span>
+                        <span className="text">
+                          {data?.created_by?.map((drc, id) => {
+                            return (
+                              <span key={id}>
+                                {drc.name}
+                                {data?.created_by.length - 1 !== id && ", "}
+                              </span>
+                            );
+                          })}
+                        </span>
+                      </div>
+                    )}
                   </div>
                 </div>
               </ContentWrapper>
