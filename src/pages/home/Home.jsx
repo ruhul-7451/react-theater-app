155b62ff1a97f5412e9a1505f8f6516bbb7912a1
ruhul-7451 <ruhulaminkhan.cse@gmail.com>
2023-06-12 22:58:25 +0600

    topRated part added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,14 @@ import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <Trending />
       <Popular />
-      <div style={{ height: 1000 }}></div>
+      <TopRated />
     </div>
   );
 };
